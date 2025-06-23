4c762e0dc26e0d6a33f7491d35a32937340c0d46
unknown <fynyourbaeid18@github.com>
2025-06-23 22:42:53 +0700

    Test hijau

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,9 @@ export const HeroSection = () => {
 
     return (
         <section>
-
+            <div>
+                <h1>Temukan Barang impian anda disini</h1>
+            </div>
         </section>
     );
 };
