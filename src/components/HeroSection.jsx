ed717b07983c96adce72c2628584ba7dc00c2b63
FaizZzhue <fynyourbaeid18@github.com>
2025-06-24 00:00:20 +0700

    add multiple folders and components

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,8 +7,17 @@ export const HeroSection = () => {
             id="#Hero"
             className="relative min-h-screen flex flex-col items-center justify-center px-4"
         >
-            <div>
-                <h1>Temukan Barang impian anda disini</h1>
+            <div className="container max-w-4xl mx-auto text-center z-10">
+                <div className="space-y-6">
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                        <span className="opacity-0 animate-fade-in"> Temukan Barang impian anda</span>
+                    </h1>
+                    <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center opacity-0 animate-fade-in-delay-1 ">
+                        <a href="#Product" className="click-button">
+                            Disini
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     );
