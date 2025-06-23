af3c34f9e832f6cc729c5f9f60f23ca6d523b1e3
unknown <fynyourbaeid18@github.com>
2025-06-23 22:49:45 +0700

    Trigger kontribusi hijau

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,10 @@
 export const HeroSection = () => {
 
     return (
-        <section>
+        <section 
+            id="#Hero"
+            className="relative min-h-screen flex flex-col items-center justify-center px-4"
+        >
             <div>
                 <h1>Temukan Barang impian anda disini</h1>
             </div>
