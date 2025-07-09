7f20974202b23df4d500c85f83261f1c1fda71ba
FaizZzhue <fynyourbaeid18@github.com>
2025-07-09 21:13:26 +0700

    Update :

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ export const Footer = () => {
     }
 
     return (
-        <footer id="/Hero" className="bg-primary">
+        <footer className="bg-[#4B3621]">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-8 lg:gap-12">
                     <div className="lg:col-span-4">
@@ -48,7 +48,7 @@ export const Footer = () => {
                                 > 
                                     <h3 className="text-lg text-white font-medium mb-4 uppercase">{category}                                       
                                     </h3>
-                                    <ul className="spay-y-3">
+                                    <ul className="space-y-3">
                                         {items.map((item, index) => (
                                             <li 
                                                 key={index}
--- src/components/home/NewsLatterSection.jsx
+++ src/components/home/NewsLatterSection.jsx
@@ -5,10 +5,10 @@ export const NewsLatterSection = () => {
     return (
         <section className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
             <div className="bg-link rounded-2xl overflow-hidden">
-                <div className="relative md:px-16 py-16 px-6 md:py-16">
+                <div className="relative md:px-16 py-16 px-6 md:py-16 ">
+
                     {/* Gradient BG */}
-                    <div className="absolute top-0 right-0 w-1/2 h-full bg-primary clip-path-slant hidden md:block">
-                    </div>
+                    <div className="absolute top-0 right-0 w-1/2 h-full bg-primary clip-path-slant hidden md:block" />
 
                     <div className="relative flex flex-col lg:flex-row items-center justify-between gap-8 lg:gap-12">
                         {/* Left Content */}
@@ -21,10 +21,10 @@ export const NewsLatterSection = () => {
                         <div className="flex flex-col sm:flex-row gap-4 sm:gap-0">
                             <input 
                                 type="email"
-                                placeholder="Enter Your Email Address"
+                                placeholder="Your Email Address"
                                 className="w-full bg-white sm:w-auto md:w-80 px-4 sm:px-6 py-3 sm:py-4 rounded-xl sm:rounded-l-xl sm:rounded-r-none focus:outline-none"
                             />
-                            <button className="w-full sm:w-auto cursor-pointer bg-link text-white px-6 sm:px-8 py-3 sm:py-4 rounded-xl sm:rounded-l-none sm:rounded-r-lg flex items-center gap-2">
+                            <button className="w-full sm:w-auto cursor-pointer bg-[#F4A261] text-white px-6 sm:px-8 py-3 sm:py-4 rounded-xl sm:rounded-l-none sm:rounded-r-lg flex items-center gap-2">
                                 <span >Discover</span>
                                 <FiArrowRight className="size-5"/>
                             </button>
